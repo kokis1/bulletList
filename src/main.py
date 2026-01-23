fe6c3cadb78b5042dcd6664c79813e683077a35d
kokis1 <reuben.stannah@icloud.com>
2026-01-23 21:35:15 +0000

    doing more examples

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from textual.app import App
+from textual.app import App, ComposeResult
 from textual import events
+from textual.widgets import Welcome, Button, Label
 
 class tuiApp(App):
    pass
@@ -24,6 +25,24 @@ class eventApp(App):
       if event.key.isdecimal():
          self.screen.styles.background = self.COLORS[int(event.key)]
 
+class WelcomeApp(App):
+    async def on_key(self) -> None:
+        await self.mount(Welcome())
+        self.query_one(Button).label = "YES!"
+
+    def on_button_pressed(self) -> None:
+        self.exit()
+
+class QuestionApp(App[str]):
+    def compose(self) -> ComposeResult:
+        yield Label("Do you love Textual?")
+        yield Button("Yes", id="yes", variant="primary")
+        yield Button("No", id="no", variant="error")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.exit(event.button.id)
+
 if __name__ == "__main__":
-   app = eventApp()
-   app.run()
+   app = QuestionApp()
+   reply = app.run()
+   print(reply)
