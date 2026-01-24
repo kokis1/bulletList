726ed3b2d1e21a1e89da7194256ee3a5760071d6
kokis1 <reuben.stannah@icloud.com>
2026-01-24 16:40:37 +0000

    displaying list of recently opened files

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-def get_recent_files():
-   with open("./metadata.txt") as file:
-      ...
+def get_recent_files(path):
+   with open(path, mode="r") as file:
+      recent_files = []
+      file_line = False # toggles on and then off when the files are there or not
+      for line in file.readlines():
+         line = line.strip("\n")
+         if file_line:
+            recent_files.append(line)
+         if line == "files":
+            file_line = True
+         if line == "end of files":
+            file_line = False
+   return recent_files
+
 def main():
    response = input("Open a new file? [y/n] ")
    if response == "y":
       response = input("File Location? [y/n] ")
    elif response == "n":
       print("recently opened files:")
+      recent_files = get_recent_files("./metadata.txt")
+      for file in recent_files:
+         print(file)
 if __name__ == "__main__":
    main()
