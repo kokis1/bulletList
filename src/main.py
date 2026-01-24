eed7ba6e0c8dff485bff50e387c31dda94c4c16c
kokis1 <reuben.stannah@icloud.com>
2026-01-24 10:38:14 +0000

    wrote some example code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
 from blessings import Terminal
 
+class task:
+   def __init__(self, description: str, duedate: str, tag: str) -> None:
+      self.description = description
+      self.duedate = duedate
+      self.tag = tag
 
 
+def main(term: Terminal) -> None:
+   # sets the terminal to full screen
+   with term.fullscreen():
+   # sets the cursor at the top left of the screen
+      term.location(0, 0)
+   # prints the menu
+   print(term.move_x(8) + "Menu")
+   print("Tasks" + term.move_right + "DueDate" + term.move_right + "Tag")
+   # asks if the user wants to load or create a new set of tasks
+   # loads the tasks if so
 
-def main():
-   ...
 if __name__ == "__main__":
-   main()
+   term = Terminal()
+   main(term)
