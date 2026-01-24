d33618f9f240e159afbe475ae56924c88d664032
kokis1 <reuben.stannah@icloud.com>
2026-01-24 16:29:18 +0000

    starting to build the first version

--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
-from blessings import Terminal
-
-class task:
-   def __init__(self, description: str, duedate: str, tag: str) -> None:
-      self.description = description
-      self.duedate = duedate
-      self.tag = tag
-
-
-def main(term: Terminal) -> None:
-   # sets the terminal to full screen
-   with term.fullscreen():
-   # sets the cursor at the top left of the screen
-      term.location(0, 0)
-   # prints the menu
-   print(term.move_x(8) + "Menu")
-   print("Tasks" + term.move_right + "DueDate" + term.move_right + "Tag")
-   # asks if the user wants to load or create a new set of tasks
-   # loads the tasks if so
-
+def get_recent_files():
+   with open("./metadata.txt") as file:
+      ...
+def main():
+   response = input("Open a new file? [y/n] ")
+   if response == "y":
+      response = input("File Location? [y/n] ")
+   elif response == "n":
+      print("recently opened files:")
 if __name__ == "__main__":
-   term = Terminal()
-   main(term)
+   main()
