b7af45e688b72bb302cfa1f0b8dae3b52b6f5c4a
kokis1 <reuben.stannah@icloud.com>
2026-01-24 16:50:12 +0000

    added a way to check if the path is correct

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 def get_recent_files(path):
    with open(path, mode="r") as file:
       recent_files = []
@@ -12,14 +14,27 @@ def get_recent_files(path):
             file_line = False
    return recent_files
 
+def valid_path(path: str) -> bool:
+   return os.path.exists(path) and os.path.isfile(path)
+
+
+def open_file(metadata_path: str) -> str:
+   active_file = ""
+   while not valid_path(active_file):
+      response = input("Open a new file? [y/n] ")
+      if response == "y":
+         print("File location:")
+         response = input("> ")
+      elif response == "n":
+         print("recently opened files:")
+         recent_files = get_recent_files(metadata_path)
+         for file in recent_files:
+            print(file)
+         response = input("File location: ")
+      active_file = response
+   return active_file
+
 def main():
-   response = input("Open a new file? [y/n] ")
-   if response == "y":
-      response = input("File Location? [y/n] ")
-   elif response == "n":
-      print("recently opened files:")
-      recent_files = get_recent_files("./metadata.txt")
-      for file in recent_files:
-         print(file)
+   open_file("./metadata.txt")
 if __name__ == "__main__":
    main()
